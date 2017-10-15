2bbd45eb1b1f285102e180423e025237821d5283
Frederick-S <mao_xiaodan@hotmail.com>
2017-10-15 11:44:53 +0800

    Update 1.12

--- chapter01/1.12.rkt
+++ chapter01/1.12.rkt
@@ -1,8 +1,11 @@
 #lang racket
 
-(define (pascal-triangle row column)
+(define (pascal-triangle-element row column)
     (if (or (= column 1) (= column row))
         1
-        (+ (pascal-triangle (- row 1) (- column 1)) (pascal-triangle (- row 1) column))))
+        (+ (pascal-triangle-element (- row 1) (- column 1)) (pascal-triangle-element (- row 1) column))))
 
-(provide pascal-triangle)
+(define (pascal-triangle row)
+    (map (lambda (column) (pascal-triangle-element row column)) (range 1 (+ row 1) 1)))
+
+(provide pascal-triangle-element pascal-triangle)
--- tests/chapter01/1.12.rkt
+++ tests/chapter01/1.12.rkt
@@ -2,7 +2,13 @@
 
 (require rackunit "../../chapter01/1.12.rkt")
 
-(check-equal? (pascal-triangle 1 1) 1 "Should return 1")
-(check-equal? (pascal-triangle 2 1) 1 "Should return 1")
-(check-equal? (pascal-triangle 3 2) 2 "Should return 2")
-(check-equal? (pascal-triangle 5 3) 6 "Should return 6")
+(check-equal? (pascal-triangle-element 1 1) 1 "Should return 1")
+(check-equal? (pascal-triangle-element 2 1) 1 "Should return 1")
+(check-equal? (pascal-triangle-element 3 2) 2 "Should return 2")
+(check-equal? (pascal-triangle-element 4 4) 1 "Should return 1")
+(check-equal? (pascal-triangle-element 5 3) 6 "Should return 6")
+(check-equal? (pascal-triangle 1) (list 1) "Should return (1)")
+(check-equal? (pascal-triangle 2) (list 1 1) "Should return (1 1)")
+(check-equal? (pascal-triangle 3) (list 1 2 1) "Should return (1 2 1)")
+(check-equal? (pascal-triangle 4) (list 1 3 3 1) "Should return (1 3 3 1)")
+(check-equal? (pascal-triangle 5) (list 1 4 6 4 1) "Should return (1 4 6 4 1)")

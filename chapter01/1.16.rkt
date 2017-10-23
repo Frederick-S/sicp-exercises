8e6fa15add692a40f72cefd13b1b88a8d98e902b
Frederick-S <mao_xiaodan@hotmail.com>
2017-10-23 14:20:16 +0800

    Add 1.16

---
+++ chapter01/1.16.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+(define (iterative-expt b n)
+    (iterative-expt-internal 1 b n))
+
+(define (iterative-expt-internal a b n)
+    (cond ((= n 1) a)
+        ((even? n) (iterative-expt-internal (* a (square b)) b (/ n 2)))
+        (else (iterative-expt-internal (* a b) b (- n 1)))))
+
+(define (even? n)
+    (= (remainder n 2) 0))
+
+(define (square n)
+    (* n n))
+
+(provide iterative-expt)

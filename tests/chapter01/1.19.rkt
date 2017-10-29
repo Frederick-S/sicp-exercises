5027689b6be0d6e33bd09b219883a5f600ebfede
Frederick-S <mao_xiaodan@hotmail.com>
2017-10-29 11:33:54 +0800

    Add 1.19

---
+++ tests/chapter01/1.19.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+(require rackunit "../../chapter01/1.19.rkt")
+
+(check-equal? (fib 1) 1 "Should return 1")
+(check-equal? (fib 3) 2 "Should return 2")
+(check-equal? (fib 4) 3 "Should return 3")
+(check-equal? (fib 5) 5 "Should return 5")
+(check-equal? (fib 6) 8 "Should return 8")

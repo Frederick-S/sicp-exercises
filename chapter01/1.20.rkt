f1ed0c2364e3b4f64fcd1b914471db0757d5b55b
Frederick-S <mao_xiaodan@hotmail.com>
2017-11-05 13:47:37 +0800

    Add 1.20

---
+++ chapter01/1.20.rkt
@@ -0,0 +1,38 @@
+#lang racket
+
+; normal-order
+(gcd 206 40)
+(gcd 40 (remainder 206 40))
+(if (= (remainder 206 40) 0) ; performed 1 time
+    40
+    (gcd (remainder 206 40) (remainder 40 (remainder 206 40))))
+(gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
+(if (= (remainder 40 (remainder 206 40)) 0) ; performed 2 times
+    (remainder 206 40)
+    (gcd (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
+(gcd (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+(if (= (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) 0) ; performed 4 times
+    (remainder 40 (remainder 206 40))
+    (gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))))
+(gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
+(if (= (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))) 0) ; performed 7 times
+    (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+    (gcd (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))) (remainder (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))))
+(remainder (remainder 206 40) (remainder 40 (remainder 206 40))) ; performed 4 times
+2
+
+; remainder operations are performed 1 + 2 + 4 + 7 + 4 = 18 times
+
+; applicative-order
+(gcd 206 40)
+(gcd 40 (remainder 206 40))
+(gcd 40 6)
+(gcd 6 (remainder 40 6))
+(gcd 6 4)
+(gcd 4 (remainder 6 4))
+(gcd 4 2)
+(gcd 2 (remainder 4 2))
+(gcd 2 0)
+2
+
+; remainder operations are performed 4 times

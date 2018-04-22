07585a8a3f9f59769c2029ef038ba289582dddce
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-22 15:17:45 +0800

    Update test

--- tests/chapter01/1.22.rkt
+++ tests/chapter01/1.22.rkt
@@ -2,4 +2,6 @@
 
 (require rackunit "../../src/chapter01/1.22.rkt")
 
-(check-equal? (search-for-primes 1001) (list 1009 1013 1019) "Should return 199")
+(check-equal? (search-for-primes 1001) (list 1009 1013 1019) "Should return '(1009 1013 1019)")
+(check-equal? (search-for-primes 10001) (list 10007 10009 10037) "Should return '(10007 10009 10037)")
+(check-equal? (search-for-primes 100001) (list 100003 100019 100043) "Should return '(100003 100019 100043)")

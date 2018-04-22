9605c4bbca745c8635c8644531bbe426de7ce089
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-22 13:49:16 +0800

    Add 1.22

---
+++ chapter01/1.22.rkt
@@ -0,0 +1,32 @@
+#lang racket
+
+(require rackunit "./1.21.rkt")
+
+(define (prime? n)
+    (= n (smallest-divisor n)))
+
+(define (runtime)
+    (current-milliseconds))
+
+(define (timed-prime-test n)
+    (newline)
+    (display n)
+    (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+    (if (prime? n)
+        (report-prime (- (runtime) start-time))
+        false))
+
+(define (report-prime elapsed-time)
+    (display " *** ")
+    (display elapsed-time))
+
+(define (search-for-primes-by-count n count)
+    (when (not (equal? count 0))
+        (if (timed-prime-test n)
+            (search-for-primes-by-count (+ n 2) (- count 1))
+            (search-for-primes-by-count (+ n 2) count))))
+
+(define (search-for-primes n)
+    (search-for-primes-by-count n 3))

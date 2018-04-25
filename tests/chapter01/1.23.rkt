2ff6734e86f5cb2e9c19e6eba470707b94a41aed
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-25 14:35:29 +0800

    Add 1.23

---
+++ tests/chapter01/1.23.rkt
@@ -0,0 +1,7 @@
+#lang racket
+
+(require rackunit "../../src/chapter01/1.23.rkt")
+
+(check-equal? (smallest-divisor 199) 199 "Should return 199")
+(check-equal? (smallest-divisor 1999) 1999 "Should return 1999")
+(check-equal? (smallest-divisor 19999) 7 "Should return 7")

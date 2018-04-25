298df5a442fe7bd68195536250cd1857c0afe463
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-25 14:49:56 +0800

    Update 1.23

--- src/chapter01/1.23.rkt
+++ src/chapter01/1.23.rkt
@@ -17,4 +17,40 @@
 (define (square n)
     (* n n))
 
+(define (prime? n)
+    (= n (smallest-divisor n)))
+
+(define (runtime)
+    (current-milliseconds))
+
+(define (timed-prime-test n)
+    (newline)
+    (display n)
+    (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+    (if (prime? n)
+        (begin
+            (report-prime (- (runtime) start-time))
+            true)
+        false))
+
+(define (report-prime elapsed-time)
+    (display " *** ")
+    (display elapsed-time))
+
+(define (search-for-primes-by-count n count)
+    (if (not (equal? count 0))
+        (if (timed-prime-test n)
+            (append (list n) (search-for-primes-by-count (+ n 2) (- count 1)))
+            (search-for-primes-by-count (+ n 2) count))
+        (list)))
+
+(define (search-for-primes n)
+    (search-for-primes-by-count n 3))
+
 (provide smallest-divisor)
+(provide search-for-primes)
+
+; The average runtime to find a prime of (search-for-primes 100000000000001) is about 77ms, 
+; and the average runtime of the old search-for-primes is 151ms, which is almost half slower than the new function

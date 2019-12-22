06f4479dd814c53d17d9c77789b5d4611f5952c7
Frederick-S <mao_xiaodan@hotmail.com>
2019-12-22 12:17:14 +0800

    Rename files

--- tests/chapter01/1.08.rkt
+++ tests/chapter01/1.08.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(require rackunit "../../src/chapter01/1.8.rkt")
+(require rackunit "../../src/chapter01/1.08.rkt")
 
 (check-true (good-enough? (cube-root 8) 8) "Should return true")
 (check-true (good-enough? (cube-root 27) 27) "Should return true")
